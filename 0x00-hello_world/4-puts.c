d2e1081777f18ca394671465e32b2970b58aad17
Oluwawatobi123 <tobi5marks@gmail.com>
2023-07-13 17:12:58 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#inc lude<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingua puzzles");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
